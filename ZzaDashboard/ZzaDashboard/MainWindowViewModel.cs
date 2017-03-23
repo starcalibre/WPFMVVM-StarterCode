942fc00c7f06aaf0e25cb03b7c31581df821919b
Adrian Hecker <starcalibre@gmail.com>
2017-03-23 13:03:18 +1100

    Create MainWindowViewModel and attach child ViewModels

---
+++ ZzaDashboard/ZzaDashboard/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZzaDashboard.Customers;
+using ZzaDashboard.OrderPrep;
+using ZzaDashboard.Orders;
+
+namespace ZzaDashboard
+{
+    class MainWindowViewModel
+    {
+        private CustomerListViewModel _customerListViewModel = new CustomerListViewModel();
+        private OrderViewModel _orderViewModel = new OrderViewModel();
+        private OrderPrepViewModel _orderPrepViewModel = new OrderPrepViewModel();
+
+        public Object CurrentViewModel { get; set; }
+    }
+}
