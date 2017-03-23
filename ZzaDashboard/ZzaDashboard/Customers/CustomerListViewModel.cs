2306498b9cef0bc5dd39c8bfb502787260e5f071
Adrian Hecker <starcalibre@gmail.com>
2017-03-23 14:17:23 +1100

    Load Customers from database and display them in CustomerListView

--- ZzaDashboard/ZzaDashboard/Customers/CustomerListViewModel.cs
+++ ZzaDashboard/ZzaDashboard/Customers/CustomerListViewModel.cs
@@ -1,12 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Zza.Data;
+using ZzaDashboard.Services;
 
 namespace ZzaDashboard.Customers
 {
     class CustomerListViewModel : BindableBase
     {
+        private ICustomersRepository _repo = new CustomersRepository();
+
+        private ObservableCollection<Customer> _customers;
+        public ObservableCollection<Customer> Customers
+        {
+            get { return _customers; }
+            set { SetProperty(ref _customers, value); }
+        }
+
+        public async void LoadCustomers()
+        {
+            Customers = new ObservableCollection<Customer>(
+                await _repo.GetCustomersAsync());
+        }
     }
 }
